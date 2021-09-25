18b2d7594fec126caf385cf88b9c95a6a82358f2
tcho <teresacho135@gmail.com>
2021-09-24 20:30:11 -0700

    creates input forms new expenses w/ styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/Expenses/NewExpense/NewExpense';
 
 import "./App.css";
 
@@ -27,6 +28,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
