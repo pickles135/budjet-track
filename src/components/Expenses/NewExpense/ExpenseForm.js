e8e1b5217e15dffca2b35e6d40eb758e669e647c
Teresa Cho <tcho@cca.edu>
2021-09-27 15:37:28 -0700

    adds a submitt handler for the form

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -19,8 +19,20 @@ function ExpenseForm() {
         setEnteredDate(event.target.value);
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault(); 
+
+        const expenseData = {
+            title: enteredTitle, 
+            amount: enteredAmount,
+            date: enteredDate
+        };
+
+        console.log(expenseData);
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}> 
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
